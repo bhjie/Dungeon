68b09604a5b4610e74a3e0a1fd3eafadaf89975f
bhjie <34655221+bhjie@users.noreply.github.com>
2018-04-17 23:01:46 +0800

    add_scene8

--- Assets/Scripts/Bridge.cs
+++ Assets/Scripts/Bridge.cs
@@ -7,14 +7,14 @@ public class Bridge : MonoBehaviour {
     public Vector3 targetpos;
     public Vector3 startpos;
     public float smoothing;
-
+    public int scriptlock = 0;
     void Start () {
         transform.position = startpos;
     }
 	
 	void FixedUpdate()
     {
-        if (locks == 1)
+        if (locks == 1 && scriptlock==0)
         {
             Vector3 vec = targetpos;
             transform.position = Vector3.Lerp(transform.position, vec, smoothing * Time.deltaTime);
@@ -25,4 +25,6 @@ public class Bridge : MonoBehaviour {
     {
         locks = 1;
     }
+
+    
 }
