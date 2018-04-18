fff4c5131c488b3891141bd7cb12c6c29cf4a9d0
bhjie <34655221+bhjie@users.noreply.github.com>
2018-04-18 17:29:10 +0800

    SCENE8_WORKING

--- Assets/Scripts/Mazefloor.cs
+++ Assets/Scripts/Mazefloor.cs
@@ -7,24 +7,17 @@ public class Mazefloor : MonoBehaviour {
     public GameObject[] movefloor;
     public float delay=1f;
     public Vector3 start;
-    public Vector3 end;
     Vector3 swap;
     private int appear = 0;     //接触这个平面的时候将变为活动状态，离开时变为静止状态
     private float timer = 0f;   //计时器，用来延迟下落
     private int stay = 0;       //表示小球是否接触这个平面
     private int locking = 0;     //在这个平面激活后只要还在这个平面上就不会发生其他滑动效果
     private int outlock = 0;
-    Bridge bri;
+    private Mazemoving bri;
     // Use this for initialization
     void Start () {
-        bri = this.gameObject.GetComponent<Bridge>();
-		for (int i=0;i<movefloor.Length;i++)
-        {
-            Bridge obj = movefloor[i].GetComponent<Bridge>();
-            obj.startpos = obj.transform.position;
-            obj.targetpos = obj.transform.position + new Vector3(0, 5, 0);
+        bri = this.gameObject.GetComponent<Mazemoving>();
 
-        }
 	}
 	
 	// Update is called once per frame
@@ -37,8 +30,9 @@ public class Mazefloor : MonoBehaviour {
     {
         for (int i = 0; i < movefloor.Length; i++)
         {
-            Bridge obj = movefloor[i].GetComponent<Bridge>();
-            obj.Appear();
+            Mazemoving obj = movefloor[i].GetComponent<Mazemoving>();
+            obj.direction = 1;
+            obj.Active();
         }
         locking = 1;
     }
@@ -47,11 +41,10 @@ public class Mazefloor : MonoBehaviour {
     {
         for (int i = 0; i < movefloor.Length; i++)
         {
-            Bridge obj = movefloor[i].GetComponent<Bridge>();
-            swap = obj.startpos;
-            obj.startpos = obj.targetpos;
-            obj.targetpos = swap;
-            obj.Appear();
+            Mazemoving obj = movefloor[i].GetComponent<Mazemoving>();
+            obj.direction = 0;
+            obj.Active();
+
         }
         locking = 0;
     }
@@ -60,14 +53,12 @@ public class Mazefloor : MonoBehaviour {
         if (collision.gameObject.CompareTag("Player"))
         {
             appear = 1;
-            bri.scriptlock = 1;
             if (locking==0)
                 Emerge();
             outlock = 1;
            
         }
     }
-
     private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -82,7 +73,6 @@ public class Mazefloor : MonoBehaviour {
         {
             appear = 0;
             timer = 0f;
-            bri.scriptlock = 0;
             StartCoroutine(MyMethod());
             outlock = 0;
         }
@@ -91,6 +81,10 @@ public class Mazefloor : MonoBehaviour {
     {
         yield return new WaitForSeconds(0.5f);
         if (locking == 1 && appear == 0)
+        {
             Landed();
+            
+        }
+            
     }
 }
---
+++ Assets/Scripts/Mazemoving.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mazemoving : MonoBehaviour {
+
+    public int direction = 1;
+    public Vector3 start;
+    public Vector3 target;
+    public float smoothing = 5F;
+    private int act = 0;
+    private int scriptlock;
+    private float timer;
+    private int delaymoving = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (direction == 1 && scriptlock == 0 && act == 1) 
+        {
+            Vector3 vec = target;
+            transform.position = Vector3.Lerp(transform.position, vec, smoothing * Time.deltaTime);
+       
+        }
+        if (direction == 0 && scriptlock == 0 && act == 1) 
+        {
+            Vector3 vec = start;
+            transform.position = Vector3.Lerp(transform.position, vec, smoothing * Time.deltaTime);
+        }
+        if (delaymoving==1 && direction == 0 && scriptlock == 0)
+        {
+            timer += Time.deltaTime;
+            if (timer>=1f)
+            {
+                Vector3 vec = start;
+                transform.position = Vector3.Lerp(transform.position, vec, smoothing * Time.deltaTime);
+            }
+            if (transform.position==start)
+            {
+                delaymoving = 0;
+            }
+        }
+    }
+
+    public void Active()
+    {
+        act = 1;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            scriptlock = 1;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            scriptlock = 0;
+            if (act==1)
+            {
+                delaymoving = 1;
+                timer = 0f;
+            }
+            act = 0;
+        }
+    }
+}
