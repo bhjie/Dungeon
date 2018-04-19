a6c1468f935dde143de168785ab0fa5ca6f800e3
bhjie <34655221+bhjie@users.noreply.github.com>
2018-04-20 00:33:34 +0800

    scene8_fin

---
+++ Assets/Scripts/MazeElevator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeElevator : MonoBehaviour {
+
+    public Vector3 point_1;
+    public Vector3 point_2;
+    private int dir=1;
+    private int open = 0;
+    public float smoothing = 10f;
+    public float delay = 0.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (open==1 && dir==1)
+        {
+            Vector3 vec = point_1;
+            transform.position = Vector3.Lerp(transform.position, vec, smoothing * Time.deltaTime);
+        }
+        if (open==1 && dir==0)
+        {
+            Vector3 vec = point_2;
+            transform.position = Vector3.Lerp(transform.position, vec, smoothing * Time.deltaTime);
+        }
+        if (dir==1 && transform.position==point_1 && open==1)
+        {
+            open = 0;
+            dir = 1 - dir;
+        }
+        if (dir == 0 && transform.position == point_2 && open==1)
+        {
+            open = 0;
+            dir = 1 - dir;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(MyMethod());
+
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        open = 1;
+    }
+
+    IEnumerator MyMethod()
+    {
+        yield return new WaitForSeconds(delay);
+        open = 1;
+        
+
+    }
+}
---
+++ Assets/Scripts/TpInScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TpInScene : MonoBehaviour {
+
+    public Vector3 target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.position = target;
+        }
+    }
+}
