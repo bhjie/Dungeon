0165078b20bf0c87690eae4f46cbdc945cd05ffc
bhjie <34655221+bhjie@users.noreply.github.com>
2018-04-22 22:25:40 +0800

    scene5_fin

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour {
+    public GameObject target;
+    public Vector3 move;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.rigidbody.velocity = new Vector3(0, 0, 0);
+            collision.transform.position = target.transform.position + move;
+        }
+    }
+}
