c3958844927785ff6312a07e8df837e4befcbf2f
bhjie <34655221+bhjie@users.noreply.github.com>
2018-05-06 18:11:44 +0800

    scene7_r

--- Assets/Scripts/SwapCamera.cs
+++ Assets/Scripts/SwapCamera.cs
@@ -5,30 +5,30 @@ using UnityEngine;
 public class SwapCamera : MonoBehaviour {
 
     // Use this for initialization
-    public GameObject[] cameras;
-    public string[] shotcuts;
-    public bool changeAudioListener = true;
-    private int cflag = 0;
+    public GameObject cameras;
+    public Vector3 offset;
+    private Vector3 temp;
 
     private void Start()
     {
-        cameras[1].GetComponent<AudioListener>().enabled = false;
-        cameras[1].GetComponent<Camera>().enabled = false;
+        temp = cameras.GetComponent<CameraController>().offset;
     }
     void Update()
     {
         
-        if (Input.GetKeyUp("q"))
-            SwitchCamera();
-    }
-
-    void SwitchCamera()
-    {
-        cameras[cflag].GetComponent<AudioListener>().enabled = false;
-        cameras[cflag].GetComponent<Camera>().enabled = false;
-        cflag = 1 - cflag;
-        cameras[cflag].GetComponent<AudioListener>().enabled = true;
-        cameras[cflag].GetComponent<Camera>().enabled = true;
         
     }
+
+    public void SwitchCamera()
+    {
+        cameras.GetComponent<CameraController>().originRotationX = 90;
+        temp = cameras.GetComponent<CameraController>().offset;
+        cameras.GetComponent<CameraController>().offset = offset;
+    }
+    public void SwitchCameraBack()
+    {
+        cameras.GetComponent<CameraController>().originRotationX = 60;
+        cameras.GetComponent<CameraController>().offset = temp;
+    }
+
 }
